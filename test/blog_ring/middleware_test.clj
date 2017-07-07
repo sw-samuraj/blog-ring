60dcc9ff4469370f0cbbad873ec9b5fafab9ba06
Vít Kotačka <vit.kotacka@gmail.com>
2017-07-07 21:35:49 +0200

    Test with a handler has been added.

--- test/blog_ring/middleware_test.clj
+++ test/blog_ring/middleware_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [blog-ring.middleware :refer :all]))
 
+(defn- handler [request]
+  {:status  200
+   :headers {}})
+
 (deftest test-no-content
   (testing "no content via identity"
-    (is (= 204 (:status ((wrap-no-content identity) {}))))))
+    (is (= 204 (:status ((wrap-no-content identity) {})))))
+
+  (testing "no content with a handler"
+    (is (= 204 (:status ((wrap-no-content handler) {}))))))
