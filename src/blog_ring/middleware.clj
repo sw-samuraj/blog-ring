9ffa5d85c171b49decc7a7288628844ecfa9c56f
Vít Kotačka <vit.kotacka@gmail.com>
2017-07-08 12:16:12 +0200

    Middleware wrap-put-allowed has been added, test included.

--- src/blog_ring/middleware.clj
+++ src/blog_ring/middleware.clj
@@ -7,3 +7,13 @@
   (fn [request]
     (let [response (handler request)]
       (assoc response :status 204))))
+
+(defn wrap-put-allowed
+  "Middleware that returns a 405 Method Not Allowed
+  if the request doesn't have :put method."
+  [handler]
+  (fn [request]
+    (if (= (:request-method request) :put)
+      (handler request)
+      (let [response (handler request)]
+        (assoc response :status 405)))))
--- test/blog_ring/middleware_test.clj
+++ test/blog_ring/middleware_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [blog-ring.middleware :refer :all]))
 
+(def put-request {:request-method :put})
+
+(def get-request {:request-method :get})
+
 (defn- handler [request]
   {:status  200
    :headers {}})
@@ -12,3 +16,10 @@
 
   (testing "no content with a handler"
     (is (= 204 (:status ((wrap-no-content handler) {}))))))
+
+(deftest test-put-allowed
+  (testing "put method returns 204"
+    (is (= 200 (:status ((wrap-put-allowed handler) put-request)))))
+
+  (testing "get method returns 405"
+    (is (= 405 (:status ((wrap-put-allowed identity) get-request))))))
